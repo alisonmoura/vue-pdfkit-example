0e572ac4a2aceb7463c9c9245faa17e0e2470216
Alison Moura <alison.oghino@gmail.com>
2020-06-17 01:27:13 -0400

    Innitial version

---
+++ src/App.vue
@@ -0,0 +1,104 @@
+<template>
+  <div id="app">
+    <theme-changer></theme-changer>
+    <div class="text-center">
+      <img alt="Vue logo" src="./assets/logo.png" />
+    </div>
+    <font-sizer></font-sizer>
+    <home></home>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+import FontSizer from './components/FontSizer.vue'
+import ThemeChanger from './components/ThemeChanger.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+    FontSizer,
+    ThemeChanger
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 65%;
+  margin: auto;
+  margin-top: 60px;
+}
+
+* {
+  outline: none;
+}
+body {
+  font-size: 100%;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+body.dark {
+  background-color: #2c3e50;
+  color: white;
+}
+h1 {
+  font-size: 2rem;
+}
+h3 {
+  font-size: 1.2rem;
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  line-height: 2.5;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+button {
+  padding: 10px 30px;
+  border: none;
+  background-color: #42b983;
+  color: white;
+  font-size: 0.8rem;
+  border-radius: 10px;
+  font-weight: bold;
+  transition: 0.2s;
+  cursor: pointer;
+}
+button:active {
+  background-color: #2c3e50;
+}
+.button-group button:first-child {
+  border-top-right-radius: 0px;
+  border-bottom-right-radius: 0px;
+}
+.button-group button:last-child {
+  border-top-left-radius: 0px;
+  border-bottom-left-radius: 0px;
+}
+.row {
+  display: flex;
+}
+.row.justify-center {
+  justify-content: center;
+}
+.row.justify-end {
+  justify-content: flex-end;
+}
+.text-center {
+  text-align: center;
+}
+.text-justify {
+  text-align: justify;
+}
+</style>
---
+++ src/components/Instructinos.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h3>Dependencies</h3>
+    <ul>
+      <li>Vue 2</li>
+      <li>PDFKit</li>
+      <li>Blob Stream</li>
+      <li>Browser File Downloader</li>
+    </ul>
+    <h3>Setup</h3>
+    <p class="instructions text-justify">
+      First of all, we'll need to change some
+      <span>webpack</span> configurations. As we are using
+      <span>Vue CLI 3</span> for scaffolding your project, we have to create the
+      <span>vue.config.js</span> file on the project's root folder:
+    </p>
+    <vue-config-snippet></vue-config-snippet>
+  </div>
+</template>
+
+<script>
+import VueConfigSnippet from '@/components/snippets/VueConfigSnippet'
+export default {
+  components: {
+    VueConfigSnippet
+  }
+}
+</script>
+
+<style scoped>
+.instructions {
+  line-height: 2;
+}
+span {
+  font-weight: bold;
+  color: #42b983;
+}
+</style>
