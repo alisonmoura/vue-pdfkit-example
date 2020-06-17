b1b6d54c2ee50c2e3174f080c7ba74b80bbe28a1
Alison Moura <alison.oghino@gmail.com>
2020-06-17 01:20:32 -0400

    First commit

---
+++ src/components/FontSizer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row justify-center button-group">
+    <button @click="increaseFont">A+</button>
+    <button @click="decreaseFont">A-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fontSize: 100
+    }
+  },
+  methods: {
+    increaseFont() {
+      this.fontSize += 10
+      document.body.style.fontSize = `${this.fontSize}%`
+    },
+    decreaseFont() {
+      this.fontSize -= 10
+      document.body.style.fontSize = `${this.fontSize}%`
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/ThemeChanger.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row justify-end">
+    <button @click="changeMode" v-if="!darkMode">Dark Mode</button>
+    <button @click="changeMode" v-else>Light Mode</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      darkMode: false
+    }
+  },
+  methods: {
+    changeMode() {
+      this.darkMode = !this.darkMode
+      if (this.darkMode) document.querySelector('body').classList.add('dark')
+      else document.querySelector('body').classList.remove('dark')
+    }
+  }
+}
+</script>
+
+<style></style>
